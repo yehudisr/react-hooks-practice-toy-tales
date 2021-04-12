8b15a944e7f5b8c231af1914efc2fe3100bcc886
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-12 18:26:33 -0400

    all deliverables

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, onDelete, onLikes}) {
+
+  const toyCollection = toys.map((toy)=> <ToyCard key={toy.id} toy={toy} onDelete={onDelete} onLikes={onLikes}/>)
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toyCollection}</div>
   );
 }
 
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({onAddToy}) {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+  })
+
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  function handleSubmit(event){
+
+    event.preventDefault()
+
+    const newToy = {
+      ...formData,
+      likes: 0
+    }
+
+    fetch("http://localhost:3001/toys", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(res => res.json())
+    .then(onAddToy)
+
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form onSubmit={handleSubmit} className="add-toy-form">
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={formData.name}
+          onChange={handleChange}
         />
         <br />
         <input
@@ -17,9 +53,11 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={formData.image}
+          onChange={handleChange}
         />
         <br />
-        <input
+        <input 
           type="submit"
           name="submit"
           value="Create New Toy"
